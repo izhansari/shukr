5f8cefc4309d83d47c14f413a353eb89f6ef7912
isansari <isansari@ncsu.edu>
2024-08-23 22:43:17 -0400

    Commit 2! So we got: - tap anywhere to increment - hold to stop (with animation over 3 seconds) - @ !timerIsActive, floating VSTACK start button at bottom so doesnt conflict with vstack for circle
    next:
    - sound
    - vibrate
    - reset counter
    - CRUD tasbeeh

--- shukr/CombinedView.swift
+++ shukr/CombinedView.swift
@@ -4,22 +4,157 @@ import SwiftData
 
 struct CombinedView: View {
     
+    // AppStorage properties
     @AppStorage("count", store: UserDefaults(suiteName: "group.betternorms.shukr.shukrWidget"))
     var tasbeeh: Int = 10
     @AppStorage("streak") var streak = 0
+    
+    // State properties
     @State private var timerIsActive = false
     @State private var timer: Timer? = nil
     @State private var selectedMinutes = 1
     @State private var startTime: Date? = nil
     @State private var currentTime = Date()
     @State private var endTime: Date? = nil
-
-    var displayedTime: String {
-        return "\(selectedMinutes)m"
-    }
+    @State private var isHolding = false
     
     var body: some View {
-        VStack(spacing: 40) {
+        ZStack {
+            VStack(spacing: 40) {
+                ZStack {
+                    CircularProgressView(progress: timerProgress())
+                        .contentShape(Circle()) // Only the circle is tappable
+                    
+                    if timerIsActive {
+                        TasbeehCountView(tasbeeh: tasbeeh)
+                    } else {
+                        Picker("Minutes", selection: $selectedMinutes) {
+                            ForEach(1..<60) { minute in
+                                Text("\(minute)m").tag(minute)
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        .frame(width: 100)
+                        .padding()
+                    }
+                }
+                .padding(.horizontal)
+            }
+            
+            // Floating Start/Stop Button
+            VStack {
+                Spacer()
+                
+                if(!timerIsActive){
+                    Button(action: toggleTimer, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundStyle(.gray.opacity(0.2))
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundStyle(.blue.opacity(0.5))
+                            Text("start")
+                                .foregroundStyle(.white)
+                                .font(.title3)
+                                .fontDesign(.rounded)
+                        }
+                        .frame(height: 50)
+                        .shadow(radius: 5)
+                    })
+                    .padding()
+                }
+            }
+            
+            VStack {
+                Spacer()
+                
+                if timerIsActive {
+                    Text("Hold to stop the timer...")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, isHolding ? 30 : 0)
+                        .opacity(isHolding ? 1 : 0)
+                        .animation(isHolding ? .easeInOut(duration: 2) : .easeOut(duration: 0.5), value: isHolding)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity) // makes the whole thing tappable. otherwises tappable area shrinks to width of CircularProgressView
+        .background(
+            Color.clear // Makes the background tappable
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    if timerIsActive {
+                        incrementTasbeeh()
+                    }
+                }
+                .onLongPressGesture(minimumDuration: 3) { isPressing in
+                    isHolding = isPressing
+                } perform: {
+                    if timerIsActive {
+                        stopTimer()
+                    }
+                    isHolding = false
+                }
+        )
+        .onAppear(perform: setupTimer)
+        .onDisappear(perform: stopTimer)
+    }
+    
+    private func toggleTimer() {
+        if timerIsActive {
+            stopTimer()
+        } else {
+            startTimer()
+        }
+    }
+    
+    private func startTimer() {
+        timerIsActive = true
+        startTime = Date()
+        endTime = Calendar.current.date(byAdding: .minute, value: selectedMinutes, to: startTime!)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            withAnimation {
+                currentTime = Date()
+            }
+            if let endTime = endTime, currentTime >= endTime {
+                stopTimer()
+                streak += 1 // Increment the streak
+            }
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        timerIsActive = false
+        endTime = nil
+    }
+    
+    private func timerProgress() -> CGFloat {
+        if let startTime = startTime, let endTime = endTime {
+            let totalTime = endTime.timeIntervalSince(startTime)
+            let elapsedTime = currentTime.timeIntervalSince(startTime)
+            return CGFloat(elapsedTime / totalTime)
+        }
+        return 0.0
+    }
+    
+    private func setupTimer() {
+        currentTime = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            currentTime = Date()
+        }
+    }
+    
+    private func incrementTasbeeh() {
+        tasbeeh = min(tasbeeh + 1, 10000) // Adjust maximum value as needed
+        WidgetCenter.shared.reloadAllTimelines()
+    }
+    
+    // Subviews integrated into CombinedView
+    struct CircularProgressView: View {
+        let progress: CGFloat
+        
+        var body: some View {
             ZStack {
                 Circle()
                     .stroke(lineWidth: 24)
@@ -41,119 +176,36 @@ struct CombinedView: View {
                             }
                     }
                 
-                if timerIsActive {
-                    Circle()
-                        .trim(from: 0, to: {
-                            if let startTime = startTime, let endTime = endTime {
-                                let totalTime = endTime.timeIntervalSince(startTime)
-                                let elapsedTime = currentTime.timeIntervalSince(startTime)
-                                return CGFloat(elapsedTime / totalTime)
-                            } else {
-                                return 0.0
-                            }
-                        }())
-                        .stroke(style: StrokeStyle(lineWidth: 24, lineCap: .round))
-                        .frame(width: 200, height: 200)
-                        .rotationEffect(.degrees(-90))
-                        .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .animation(.spring(), value: currentTime)
-                    
-                    ZStack {
-                        // The circles beneath the text
-                        HStack(spacing: 5) {
-                            let circlesCount = tasbeeh / 100
-                            ForEach(0..<circlesCount, id: \.self) { _ in
-                                Circle()
-                                    .fill(Color.gray.opacity(0.5))
-                                    .frame(width: 10, height: 10)
-                            }
-                        }
-                        .offset(y: 40) // Position the circles below the text
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(style: StrokeStyle(lineWidth: 24, lineCap: .round))
+                    .frame(width: 200, height: 200)
+                    .rotationEffect(.degrees(-90))
+                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .animation(.spring(), value: progress)
+            }
+        }
+    }
+    
+    struct TasbeehCountView: View {
+        let tasbeeh: Int
+        
+        var body: some View {
+            ZStack {
+                HStack(spacing: 5) {
+                    let circlesCount = tasbeeh / 100
+                    ForEach(0..<circlesCount, id: \.self) { _ in
+                        Circle()
+                            .fill(Color.gray.opacity(0.5))
+                            .frame(width: 10, height: 10)
+                    }
+                }
+                .offset(y: 40) // Position the circles below the text
 
-                        // The centered text
-                        Text("\(tasbeeh % 100)")
-                            .font(.largeTitle)
-                            .bold()
-                    }
-                } else {
-                    Picker("Minutes", selection: $selectedMinutes) {
-                        ForEach(1..<60) { minute in
-                            Text("\(minute)m").tag(minute)
-                        }
-                    }
-                    .pickerStyle(WheelPickerStyle())
-                    .frame(width: 100)
-                    .padding()
-                }
+                Text("\(tasbeeh % 100)")
+                    .font(.largeTitle)
+                    .bold()
             }
-            .padding(.horizontal)
-            
-            HStack(spacing: 60) {
-                if timerIsActive {
-                    Button(action: {
-                        tasbeeh = max(tasbeeh - 1, 0)
-                        WidgetCenter.shared.reloadAllTimelines()
-                    }, label: {
-                        Image(systemName: "minus").font(.title)
-                    })
-                    
-                    Button(action: {
-                        tasbeeh = min(tasbeeh + 1, 10000) // Adjust maximum value as needed
-                        WidgetCenter.shared.reloadAllTimelines()
-                    }, label: {
-                        Image(systemName: "plus").font(.title)
-                    })
-                }
-            }
-            
-            Button(action: {
-                if timerIsActive {
-                    // Stop the timer
-                    timer?.invalidate()
-                    timer = nil
-                    timerIsActive = false
-                    endTime = nil
-                } else {
-                    // Start the timer
-                    timerIsActive = true
-                    startTime = Date()
-                    endTime = Calendar.current.date(byAdding: .minute, value: selectedMinutes, to: startTime!)
-                    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                        withAnimation {
-                            currentTime = Date()
-                        }
-                        if let endTime = endTime, currentTime >= endTime {
-                            // Timer has reached zero
-                            timer?.invalidate()
-                            timer = nil
-                            timerIsActive = false
-                            streak += 1 // Increment the streak
-                        }
-                    }
-                }
-            }, label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(.gray.opacity(0.2))
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(timerIsActive ? .black.opacity(0.5) : .blue.opacity(0.5))
-                    Text(timerIsActive ? "stop" : "start")
-                        .foregroundStyle(.white)
-                        .font(.title3)
-                        .fontDesign(.rounded)
-                }
-                .frame(height: 50)
-            })
-            .padding()
-        }
-        .onAppear {
-            currentTime = Date()
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                currentTime = Date()
-            }
-        }
-        .onDisappear {
-            timer?.invalidate()
         }
     }
 }
--- shukr/ContentView.swift
+++ shukr/ContentView.swift
@@ -1,88 +1,88 @@
+////
+////  ContentView.swift
+////  shukr
+////
+////  Created by Izhan S Ansari on 8/3/24.
+////
 //
-//  ContentView.swift
-//  shukr
+//import SwiftUI
+//import SwiftData
+//import WidgetKit
 //
-//  Created by Izhan S Ansari on 8/3/24.
+//struct ContentView: View {
+//    @AppStorage("count", store: UserDefaults(suiteName: "group.betternorms.shukr.shukrWidget"))
+//    var tasbeeh: Int = 0
+//    var timerIsActive = false
+//    @State var selectedMinutes = 1
 //
-
-import SwiftUI
-import SwiftData
-import WidgetKit
-
-struct ContentView: View {
-    @AppStorage("count", store: UserDefaults(suiteName: "group.betternorms.shukr.shukrWidget"))
-    var tasbeeh: Int = 0
-    var timerIsActive = false
-    @State var selectedMinutes = 1
-
-    var body: some View {
-        Spacer()
-        VStack(spacing: 40) {
-            ZStack{
-                Circle()
-                    .stroke(lineWidth: 24)
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(.white)
-                    .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
-                Circle()
-                    .stroke(lineWidth: 0.34)
-                    .frame(width: 175, height: 175)
-                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.3), .clear]), startPoint: .bottomTrailing, endPoint: .topLeading))
-                    .overlay {
-                        Circle()
-                            .stroke(.black.opacity(0.1), lineWidth: 2)
-                            .blur(radius: 5)
-                            .mask {
-                                Circle()
-                                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                            }
-                    }
-                
-                Circle()
-                    .trim(from: 0, to: CGFloat(tasbeeh) / 100)
-                    .stroke(style: StrokeStyle(lineWidth: 24, lineCap: .round))
-                    .frame(width: 200, height: 200)
-                    .rotationEffect(.degrees(-90))
-                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .animation(.spring(), value: tasbeeh)
-                Text("\(tasbeeh)").bold().font(.largeTitle)
-            }
-            HStack(spacing: 60) {
-                Button(action: {
-                    tasbeeh = max(tasbeeh - 1, 0)
-                    WidgetCenter.shared.reloadAllTimelines()
-                }, label: {
-                    Image(systemName: "minus").font(.title)
-                })
-                Button(action: {
-                    tasbeeh = min(tasbeeh + 1, 100)
-                    WidgetCenter.shared.reloadAllTimelines()
-                }, label: {
-                    Image(systemName: "plus").font(.title)
-                })
-            }
-        }
-        .font(.largeTitle)
-        .padding()
-        Spacer()
-        HStack{
-            if(!timerIsActive){
-                Picker("Minutes", selection: $selectedMinutes) {
-                    ForEach(1..<60) { minute in
-                        Text("\(minute)m").tag(minute)
-                    }
-                }
-                .pickerStyle(WheelPickerStyle())
-                .frame(width: 100)
-                .padding()
-            }
-        }
-    }
-}
-
-
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-        
-}
+//    var body: some View {
+//        Spacer()
+//        VStack(spacing: 40) {
+//            ZStack{
+//                Circle()
+//                    .stroke(lineWidth: 24)
+//                    .frame(width: 200, height: 200)
+//                    .foregroundColor(.white)
+//                    .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
+//                Circle()
+//                    .stroke(lineWidth: 0.34)
+//                    .frame(width: 175, height: 175)
+//                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.3), .clear]), startPoint: .bottomTrailing, endPoint: .topLeading))
+//                    .overlay {
+//                        Circle()
+//                            .stroke(.black.opacity(0.1), lineWidth: 2)
+//                            .blur(radius: 5)
+//                            .mask {
+//                                Circle()
+//                                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .topLeading, endPoint: .bottomTrailing))
+//                            }
+//                    }
+//                
+//                Circle()
+//                    .trim(from: 0, to: CGFloat(tasbeeh) / 100)
+//                    .stroke(style: StrokeStyle(lineWidth: 24, lineCap: .round))
+//                    .frame(width: 200, height: 200)
+//                    .rotationEffect(.degrees(-90))
+//                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+//                    .animation(.spring(), value: tasbeeh)
+//                Text("\(tasbeeh)").bold().font(.largeTitle)
+//            }
+//            HStack(spacing: 60) {
+//                Button(action: {
+//                    tasbeeh = max(tasbeeh - 1, 0)
+//                    WidgetCenter.shared.reloadAllTimelines()
+//                }, label: {
+//                    Image(systemName: "minus").font(.title)
+//                })
+//                Button(action: {
+//                    tasbeeh = min(tasbeeh + 1, 100)
+//                    WidgetCenter.shared.reloadAllTimelines()
+//                }, label: {
+//                    Image(systemName: "plus").font(.title)
+//                })
+//            }
+//        }
+//        .font(.largeTitle)
+//        .padding()
+//        Spacer()
+//        HStack{
+//            if(!timerIsActive){
+//                Picker("Minutes", selection: $selectedMinutes) {
+//                    ForEach(1..<60) { minute in
+//                        Text("\(minute)m").tag(minute)
+//                    }
+//                }
+//                .pickerStyle(WheelPickerStyle())
+//                .frame(width: 100)
+//                .padding()
+//            }
+//        }
+//    }
+//}
+//
+//
+//#Preview {
+//    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+//        
+//}
